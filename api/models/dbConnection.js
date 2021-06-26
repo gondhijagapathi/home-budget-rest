6a6420e8272821170c9d36981af847e100596be9
JG067071 <jg067071@cerner.net>
2021-06-26 11:31:36 +0530

    Db connection pool created

--- api/models/dbConnection.js
+++ api/models/dbConnection.js
@@ -1,14 +1,37 @@
 var mysql = require('mysql');
 
-    var connection = mysql.createConnection({
-        host: '192.168.1.2',
-        port: 3307,
-        user: 'root',
-        password: 'jagapathi',
-        database: 'homedb',
-        insecureAuth: true
-    });
+var pool = mysql.createPool({
+    host     : '192.168.1.2',
+    user     : 'root',
+    password : 'jagapathi',
+    database : 'homedb',
+    port     : 3307
+});
 
-connection.connect();
-
-module.exports = connection;
+module.exports = {
+        query: function(){
+            var sql_args = [];
+            var args = [];
+            for(var i=0; i<arguments.length; i++){
+                args.push(arguments[i]);
+            }
+            var callback = args[args.length-1]; //last arg is callback
+            pool.getConnection(function(err, connection) {
+            if(err) {
+                    console.log(err);
+                    return callback(err);
+                }
+                if(args.length > 2){
+                    sql_args = args[1];
+                }
+            connection.query(args[0], sql_args, function(err, results) {
+              connection.release(); // always put connection back in pool after last query
+              if(err){
+                        console.log(err);
+                        return callback(err);
+                    }
+              callback(null, results);
+            });
+          });
+        }
+    };
